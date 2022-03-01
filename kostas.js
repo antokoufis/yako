4593b8db943864e3f0506aa3e431e80d0595d464
Antonis Koufis <antokoufis@gmail.com>
2022-03-01 13:32:30 +0200

    Add files via upload

---
+++ kostas.js
@@ -0,0 +1,227 @@
+if(
+    (
+    new Date([item-1239_value]).getTime()<=
+    new Date(new Date().getFullYear(),11-1,1).setDate((((10-new Date(new Date().getFullYear(),11-1,1).getDay()%7))+3)%7 +1)
+    )
+    &&
+    (
+    (new Date([item-1239_value]).getTime() >=
+    new Date(new Date().getFullYear(),5-1,1).setDate((((10-new Date(new Date().getFullYear(),5-1,1).getDay()%7))+3)%7 +1))
+    &&
+    (new Date([item-1239_value]).getTime() <=
+    new Date(new Date().getFullYear(),5-1,new Date(new Date().getFullYear(), 5, 0).getDate() - new Date(new Date().getFullYear(), 5, 0).getDay()-8)
+    )
+    ||
+    (
+    (new Date([item-1239_value]).getTime()>=
+    new Date(new Date().getFullYear(),10-1,1).setDate((((10-new Date(new Date().getFullYear(), 10 -1,1).getDay()%7))+3)%7 +15)
+    )
+    &&
+    (
+    new Date([item-1239_value]).getTime()<=
+    new Date(new Date().getFullYear(),11-1,1).setDate((((10-new Date(new Date().getFullYear(),11-1,1).getDay()%7))+3)%7 +1)
+    )
+    )
+    )
+    )
+    {
+    4800
+    }
+    
+    if(
+    (
+    new Date([item-1239_value]).getTime()<=
+    new Date(new Date().getFullYear(),11-1,1).setDate((((10-new Date(new Date().getFullYear(),11-1,1).getDay()%7))+3)%7 +1)
+    )
+    &&
+    (
+    (
+    (
+    new Date([item-1239_value]).getTime() >=
+    (new Date(new Date().getFullYear(),5-1,new Date(new Date().getFullYear(), 5, 0).getDate() - new Date(new Date().getFullYear(), 5, 0).getDay()-1))
+    ))
+    &&
+    (new Date([item-1239_value]).getTime() <=
+    new Date(new Date().getFullYear(),6-1,new Date(new Date().getFullYear(), 6, 0).getDate() - new Date(new Date().getFullYear(), 6, 0).getDay()-8)
+    )
+    ||
+    (
+    (new Date([item-1239_value]).getTime() >=
+    new Date(new Date().getFullYear(),9-1,new Date(new Date().getFullYear(), 9, 0).getDate() - new Date(new Date().getFullYear(), 9, 0).getDay()-1)
+    )
+    &&
+    (new Date([item-1239_value]).getTime() <=
+    new Date(new Date().getFullYear(),10-1,1).setDate((((10-new Date(new Date().getFullYear(),10-1,1).getDay()%7))+3)%7 +8)
+    )
+    )
+    )
+    )
+    {
+    5700
+    }
+    
+    if(
+    (
+    new Date([item-1239_value]).getTime()<=
+    new Date(new Date().getFullYear(),11-1,1).setDate((((10-new Date(new Date().getFullYear(),11-1,1).getDay()%7))+3)%7 +1)
+    )
+    &&
+    (
+    (
+    (new Date([item-1239_value]).getTime() >=
+    new Date(new Date().getFullYear(),6-1,new Date(new Date().getFullYear(), 6, 0).getDate() - new Date(new Date().getFullYear(), 6, 0).getDay()-1)
+    )
+    &&
+    (new Date([item-1239_value]).getTime() <=
+    new Date(new Date().getFullYear(),7-1,new Date(new Date().getFullYear(), 7, 0).getDate() - new Date(new Date().getFullYear(), 7, 0).getDay()-8)
+    )
+    )
+    ||
+    (
+    (new Date([item-1239_value]).getTime() >=
+    new Date(new Date().getFullYear(),8-1,new Date(new Date().getFullYear(), 8, 0).getDate() - new Date(new Date().getFullYear(), 8, 0).getDay()-1)
+    )
+    &&
+    (new Date([item-1239_value]).getTime() <=
+    new Date(new Date().getFullYear(),9-1,new Date(new Date().getFullYear(), 9, 0).getDate() - new Date(new Date().getFullYear(), 9, 0).getDay()-8)
+    )
+    )
+    )
+    )
+    {
+    6500
+    }
+    
+    if(
+    (
+    new Date([item-1239_value]).getTime()<=
+    new Date(new Date().getFullYear(),11-1,1).setDate((((10-new Date(new Date().getFullYear(),11-1,1).getDay()%7))+3)%7 +1)
+    )
+    &&
+    (
+    (new Date([item-1239_value]).getTime() >=
+    new Date(new Date().getFullYear(),7-1,new Date(new Date().getFullYear(), 7, 0).getDate() - new Date(new Date().getFullYear(), 7, 0).getDay()-1)
+    )
+    &&
+    (new Date([item-1239_value]).getTime() <=
+    new Date(new Date().getFullYear(),8-1,new Date(new Date().getFullYear(), 8, 0).getDate() - new Date(new Date().getFullYear(), 5, 0).getDay()-8)
+    )
+    )
+    ){
+    7400
+    }
+    
+    
+    
+    if(
+    (
+    new Date([item-1239_value]).getTime()>
+    new Date(new Date().getFullYear(),11-1,1).setDate((((10-new Date(new Date().getFullYear(),11-1,1).getDay()%7))+3)%7 +1)
+    )
+    &&
+    (
+    (new Date([item-1239_value]).getTime() >=
+    new Date(new Date().getFullYear()+1,5-1,1).setDate((((10-new Date(new Date().getFullYear()+1,5-1,1).getDay()%7))+3)%7 +1))
+    &&
+    (new Date([item-1239_value]).getTime() <=
+    new Date(new Date().getFullYear()+1,5-1,new Date(new Date().getFullYear()+1, 5, 0).getDate() - new Date(new Date().getFullYear()+1, 5, 0).getDay()-8)
+    )
+    ||
+    (
+    (new Date([item-1239_value]).getTime()>=
+    new Date(new Date().getFullYear()+1,10-1,1).setDate((((10-new Date(new Date().getFullYear()+1, 10 -1,1).getDay()%7))+3)%7 +15)
+    )
+    &&
+    (
+    new Date([item-1239_value]).getTime()<=
+    new Date(new Date().getFullYear()+1,11-1,1).setDate((((10-new Date(new Date().getFullYear()+1,11-1,1).getDay()%7))+3)%7 +1)
+    )
+    )
+    )
+    )
+    {
+    4800
+    }
+    
+    if(
+    (
+    new Date([item-1239_value]).getTime()>
+    new Date(new Date().getFullYear(),11-1,1).setDate((((10-new Date(new Date().getFullYear(),11-1,1).getDay()%7))+3)%7 +1)
+    )
+    &&
+    (
+    (
+    (
+    new Date([item-1239_value]).getTime() >=
+    (new Date(new Date().getFullYear()+1,5-1,new Date(new Date().getFullYear()+1, 5, 0).getDate() - new Date(new Date().getFullYear()+1, 5, 0).getDay()-1))
+    ))
+    &&
+    (new Date([item-1239_value]).getTime() <=
+    new Date(new Date().getFullYear()+1,6-1,new Date(new Date().getFullYear()+1, 6, 0).getDate() - new Date(new Date().getFullYear()+1, 6, 0).getDay()-8)
+    )
+    ||
+    (
+    (new Date([item-1239_value]).getTime() >=
+    new Date(new Date().getFullYear()+1,9-1,new Date(new Date().getFullYear()+1, 9, 0).getDate() - new Date(new Date().getFullYear()+1, 9, 0).getDay()-1)
+    )
+    &&
+    (new Date([item-1239_value]).getTime() <=
+    new Date(new Date().getFullYear()+1,10-1,1).setDate((((10-new Date(new Date().getFullYear()+1,10-1,1).getDay()%7))+3)%7 +8)
+    )
+    )
+    )
+    )
+    {
+    5700
+    }
+    
+    if(
+    (
+    new Date([item-1239_value]).getTime()>
+    new Date(new Date().getFullYear(),11-1,1).setDate((((10-new Date(new Date().getFullYear(),11-1,1).getDay()%7))+3)%7 +1)
+    )
+    &&
+    (
+    (
+    (new Date([item-1239_value]).getTime() >=
+    new Date(new Date().getFullYear()+1,6-1,new Date(new Date().getFullYear()+1, 6, 0).getDate() - new Date(new Date().getFullYear()+1, 6, 0).getDay()-1)
+    )
+    &&
+    (new Date([item-1239_value]).getTime() <=
+    new Date(new Date().getFullYear()+1,7-1,new Date(new Date().getFullYear()+1, 7, 0).getDate() - new Date(new Date().getFullYear()+1, 7, 0).getDay()-8)
+    )
+    )
+    ||
+    (
+    (new Date([item-1239_value]).getTime() >=
+    new Date(new Date().getFullYear()+1,8-1,new Date(new Date().getFullYear()+1, 8, 0).getDate() - new Date(new Date().getFullYear()+1, 8, 0).getDay()-1)
+    )
+    &&
+    (new Date([item-1239_value]).getTime() <=
+    new Date(new Date().getFullYear()+1,9-1,new Date(new Date().getFullYear()+1, 9, 0).getDate() - new Date(new Date().getFullYear()+1, 9, 0).getDay()-8)
+    )
+    )
+    )
+    )
+    {
+    6500
+    }
+    
+    if(
+    (
+    new Date([item-1239_value]).getTime()>
+    new Date(new Date().getFullYear(),11-1,1).setDate((((10-new Date(new Date().getFullYear(),11-1,1).getDay()%7))+3)%7 +1)
+    )
+    &&
+    (
+    (new Date([item-1239_value]).getTime() >=
+    new Date(new Date().getFullYear()+1,7-1,new Date(new Date().getFullYear()+1, 7, 0).getDate() - new Date(new Date().getFullYear()+1, 7, 0).getDay()-1)
+    )
+    &&
+    (new Date([item-1239_value]).getTime() <=
+    new Date(new Date().getFullYear()+1,8-1,new Date(new Date().getFullYear()+1, 8, 0).getDate() - new Date(new Date().getFullYear()+1, 5, 0).getDay()-8)
+    )
+    )
+    ){
+    7400
+    }
---
+++ min-js.js
@@ -0,0 +1,40 @@
+7900
+var selected = new Date([item-1239_value]);
+
+var season_A_Start = new Date('2/04/22');
+var season_A_End = new Date('27/05/22');
+
+
+var season_A_Start = new Date('04/02/2022');
+var season_A_End = new Date('05/27/2022');
+
+var season_B_Start = new Date('05/28/2022');
+var season_B_End = new Date('07/15/2022');
+
+var season_C_Start = new Date('07/16/2022');
+var season_C_End = new Date('08/26/2022');
+
+var season_D_Start = new Date('08/27/2022');
+var season_D_End = new Date('09/30/2022');
+
+var season_E_Start = new Date('10/01/2022');
+var season_E_End = new Date('10/28/2022');
+
+if(selected>=season_A_Start && selected<=season_A_End){
+7900
+}
+
+if(selected>=season_B_Start && selected<=season_B_End){
+8900
+}
+
+if(selected>=season_C_Start && selected<=season_C_End){
+ 9900
+}
+
+if(selected>=season_D_Start && selected<=season_D_End){
+8900;
+}
+if(selected>=season_E_Start && selected<=season_E_End){
+7900;
+}
